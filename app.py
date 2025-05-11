077c92c58071717ae7def233b1266f263ea7a8a4
aristit0 <aristitosugihharto@gmail.com>
2025-05-11 20:20:40 +0700

    Clean commit without secrets

--- app.py
+++ app.py
@@ -19,5 +19,8 @@ def chat():
     reply = response["choices"][0]["message"]["content"]
     return jsonify({"reply": reply})
 
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    # port = int(os.environ.get("PORT",8080))
+    # app.run(host='127.0.0.1', port=int(os.environ['CDSW_APP_PORT']))
+    app.run(debug=True, port=5001)
